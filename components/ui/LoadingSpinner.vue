7bbe246cbfbde5c1cb7cce563db57f5371572b0c
Xian Zhang <xian.z512@gmail.com>
2022-05-29 03:40:52 -0700

    add paper background for some pages

--- components/ui/LoadingSpinner.vue
+++ components/ui/LoadingSpinner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center fixed-center" style="opacity: 0.9; z-index: 100">
+  <div class="text-center fixed-center" style="opacity: 0.9; z-index: 100;">
     <b-spinner label="Spinning" variant="danger"></b-spinner>
     <p>Loading</p>
   </div>
